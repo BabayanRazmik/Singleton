0b072f350264c04e9acd1c96e292b9df00b533df
Razmik Babayan <razmik.babayan@7smarts.com>
2023-09-01 13:45:26 +0400

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,44 @@
+﻿
+////////////////
+///SINGLETON///
+//////////////
+
+class Singleton
+{
+    static Singleton uniqueInstance;
+    string singletondata = string.Empty;
+    protected Singleton()
+    {
+    }
+    // only 1 instance
+    public static Singleton Instance()
+    {
+        if (uniqueInstance == null)
+        {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+    public void SingletonOperation()
+    {
+        singletondata = "SingletonData";
+    }
+    public string GetSingletonData()
+    {
+        return singletondata;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Singleton instance1 = Singleton.Instance();
+        Singleton instance2 = Singleton.Instance();
+        Console.WriteLine("Is  the instance1 and instance2 has the same referance -> " + ReferenceEquals(instance1, instance2));
+
+        instance1.SingletonOperation();
+        string singletonData =instance1.GetSingletonData();
+        Console.WriteLine(singletonData);
+    }
+}
